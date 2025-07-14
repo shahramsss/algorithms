113dc1c55f0f5c63c0f53090269946b1f83c5221
shahramsss <shahramsss1987@gmail.com>
2025-07-14 15:17:01 +0330

    add find min rotate

---
+++ codes/find_min_rotate.py
@@ -0,0 +1,21 @@
+"""
+[0,1,2,3,4,5,6,7] => [4,5,6,7,0,1,2]
+find min element?
+"""
+
+
+def find_min_rotate(array):
+    low = 0
+    high = len(array) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if array[mid] > array[high]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return array[low]
+
+
+print(find_min_rotate([6,7,8,9,11,12,13,3,4,5,]))
